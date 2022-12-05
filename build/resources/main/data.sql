3078f2c71176392bbcda8165b18e43647ed3662c
Machikanewani <85107247+Machikanewani@users.noreply.github.com>
2022-12-05 11:02:51 +0900

    Delete build directory

--- build/resources/main/data.sql
+++
@@ -1,4 +0,0 @@
-insert into issues(summary, description) values ('線形代数A', '逆行列を求めよう');
-insert into issues(summary, description) values ('情報科学演習C', 'TCP/IPを学ぼう');
-insert into issues(summary, description) values ('情報論A', 'ハフマン符号について');
-insert into issues(summary, description) values ('情報解析B', 'NP困難の近似解');
